05ed711da9e223bb75239b1ef30b7f45417c9ce5
aba2222 <ABCSSS2021@outlook.com>
2025-08-20 12:01:41 +0800

    Update websocket

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, Request, WebSocket
+from typing import List
+from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 from fastapi.templating import Jinja2Templates
+from fastapi.websockets import WebSocketState
 
 app = FastAPI()
 
@@ -13,12 +15,30 @@ def read_root(request: Request):
     )
 
 
+active_connections: List[WebSocket] = []
+
+
 @app.websocket("/ws/{game_id}")
 async def websocket_endpoint(websocket: WebSocket, game_id: str):
     await websocket.accept()
+    active_connections.append(websocket)
+
     try:
         while True:
             data = await websocket.receive_json()
-            await websocket.send_text(f"Message received for game {game_id}: {data}")
+            message = f"Message received for game {game_id}: {data}"
+            for connection in active_connections:
+                if connection != websocket:
+                    await connection.send_text(message)
+
+    except WebSocketDisconnect as e:
+        print(f"Connection closed with code {e.code}")
+        active_connections.remove(websocket)
     except Exception as e:
-        await websocket.close(code=1000, reason=str(e))
+        await websocket.send_text(f"Error occurred: {str(e)}")
+        print(f"Error: {str(e)}")
+    finally:
+        if websocket in active_connections:
+            active_connections.remove(websocket)
+        if not websocket.client_state == WebSocketState.DISCONNECTED:
+            await websocket.close(code=1000, reason="Normal closure")
